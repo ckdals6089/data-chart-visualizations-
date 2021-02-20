5256efd013e033b9e8dc9345f32c68c7e738b3ae
Changmin Shin <ckdals6089@gmail.com>
2021-02-20 03:08:35 -0500

    Create circle chart for score

--- src/components/GaugeCard.js
+++ src/components/GaugeCard.js
@@ -1,12 +1,37 @@
+import { text } from '@fortawesome/fontawesome-svg-core';
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { CircularProgressbar } from 'react-circular-progressbar';
 const GaugeCard = ({ gauge }) => {
   const { name, score, vsly, sample } = gauge;
+  console.log(gauge);
   return (
     <div className="card-container">
-      <span>{name}</span>
-      <span className="score">{score}</span>
-      <span>Sample: {sample}</span>
+      <span className="name">{name}</span>
+      <CircularProgressbar
+        className="card-circle"
+        value={score}
+        styles={{
+          path: {
+            stroke: `rgba(62, 152, 199, ${score / 100})`,
+          },
+          trail: {
+            stroke: '#d6d6d6',
+            transform: 'rotate(0.25turn)',
+            transformOrigin: 'center center',
+          },
+          text: {
+            fontSize: '16px',
+          },
+        }}
+      />
+      <span className="score">{`${score}%`}</span>
+      {!vsly ? (
+        <span className="vsly">null</span>
+      ) : (
+        <span className="vsly">{vsly}</span>
+      )}
+
+      <span className="sample">Sample: {sample}</span>
     </div>
   );
 };
